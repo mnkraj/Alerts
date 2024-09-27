db40802df1f865dad0a15f4b8e26aaa86a7345da
Mayank Raj <mayankraj7100@gmail.com>
2024-09-27 21:29:15 +0530

    first

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const twilio = require('twilio');
+const dotenv = require("dotenv");
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+
+const noticesUrl = process.env.n_url;
+let latestNoticeId = 6540;
+
+async function fetchNotices() {
+    try {
+        const response = await axios.get(noticesUrl);
+        const notices = response.data.data;
+
+        if (notices.length > 0) {
+            const newNotices = [];
+            
+            for (const notice of notices) {
+                if (!latestNoticeId || notice.id > latestNoticeId) {
+                    newNotices.push(notice);
+                } else {
+                    break;  // Stop if we reach the last processed notice
+                }
+            }
+
+            if (newNotices.length > 0) {
+                newNotices.reverse(); // Reverse to send the oldest notice first
+                newNotices.forEach(sendSmsNotification);
+                latestNoticeId = newNotices[0].id; // Update the latestNoticeId to the most recent
+            }
+        }
+    } catch (error) {
+        console.error('Error fetching notices. Institute server may be down.', error.message);
+        return;
+    }
+}
+
+function sendSmsNotification(notice) {
+    const messageBody = `New Notice: ${notice.title}\nLink: ${notice.path}`;
+
+    client.messages
+        .create({
+            body: messageBody,
+            from: process.env.TWILIO_PHONE_NUMBER,
+            to: process.env.MY_PHONE_NUMBER
+        })
+        .then(message => console.log('SMS sent:', message.sid))
+        .catch(error => console.error('Error sending SMS:', error));
+}
+
+const interval =   process.env.intervel;
+
+function retryFetchNotices(retries = 3, delay = 5000) {
+    fetchNotices()
+        .catch(error => {
+            if (retries > 0) {
+                console.log(`Retrying... Attempts left: ${retries}`);
+                setTimeout(() => retryFetchNotices(retries - 1, delay), delay);
+            } else {
+                console.error('Max retry attempts reached. Skipping this round.');
+            }
+        });
+}
+
+setInterval(() => retryFetchNotices(3, 5000), interval);
+
+app.get("/", (req, res) => {
+  res.send("kya aapke tooth paste mein namak hai ?");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
